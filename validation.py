bea39caf3fcb80ae40a4bea9126d57d56cb1a729
Tharun Reddy <77614000+Reddytharun266@users.noreply.github.com>
2022-12-02 11:11:29 +0530

    Add files via upload

--- validation.py
+++ validation.py
@@ -4,9 +4,9 @@ def validate():
 	print("Authentication Required!\n")
 	number = input("Enter your mobile number: ")
 	OTP = random.randint(1000, 9999)
-	print("OTP: ", OTP)
+	print('"OTP sent to', number, "is" , OTP, '"')
 	message = str(OTP)
-	url = "https://www.fast2sms.com/dev/bulkV2?authorization=9Py6U8i5SmHM1FCeqDEnVOB7w4zculxTtNjaR2gYpoJdKsZfbXgdEKZ4w7OIHGlQLk80FT3YDzrsmCyx&language=english&route=q&numbers=" + number + "&message=" + message;
+	url = "https://www.fast2sms.com/dev/bulkV2?authorization='key'&language=english&route=q&numbers=" + number + "&message=" + message;
 	response = requests.get(url)
 	recived_OTP = int(input("Enter OTP: "))
 	if(OTP == recived_OTP):
